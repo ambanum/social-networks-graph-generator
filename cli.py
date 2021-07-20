cf5d99e1300f9f77b852c317d6549755b665682f
Antoine-Barre <53567231+Antoine-Barre@users.noreply.github.com>
2021-07-20 17:05:30 +0200

    Fix : added some comments and help

--- cli.py
+++ cli.py
@@ -12,12 +12,16 @@ import click
 
 @click.command()
 @click.argument('search')
-@click.option('--since')
-@click.option('--maxresults')
-@click.option('retweets_minimal')
+@click.option('--since', help="The date up to which we will look for tweets")
+@click.option('--maxresults', help="The maximal number of tweets that we will look at")
+@click.option('--retweets_minimal', help="The minimal number of retweets a tweet must have for we to fetch its retweeter")
 
 def main(search, since, maxresults, retweets_minimal):
-    """Command line utility to estimate the probability of a twitter user to be a bot"""
+    """Command line utility that print the json of a graph constructed from a hashtag or expression
+    The graph is constructed in the following way :
+    We fetch the tweets according to the options
+    With these tweets we create nodes for every user who has tweeted something
+    And then we add links between two users whenever one user has retweeted another"""
     print(graph_cli(search, date=since, maxresults=maxresults, retweets_minimal=retweets_minimal))
     
 if __name__ == "__main__":
