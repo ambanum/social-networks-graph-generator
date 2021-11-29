dd2a0a363ff2d47e5949638b2e1597804953af29
antoine <antoine.yor@gmail.com>
2021-11-29 09:53:35 +0100

    add sncscrape to requirements

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "click==8.0.1",
         "pytz==2021.3",
         "matplotlib==3.5.0",
-        "-e git+https://github.com/JustAnotherArchivist/snscrape.git@a6b6f3faaa26f541d9469651451340096b5abc92#egg=snscrape"
+        "snscrape @ git+https://github.com/JustAnotherArchivist/snscrape.git@a6b6f3faaa26f541d9469651451340096b5abc92#egg=snscrape"
 
     ],
     dependency_links=["git+https://github.com/JustAnotherArchivist/snscrape.git"],
